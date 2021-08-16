f8fcd8bbde024091769063e5c78872ea199f7a68
98chaithanya <chaithanyarajan998@gmail.com>
2021-08-16 13:29:26 +0530

    first

---
+++ sample.js
@@ -0,0 +1,10 @@
+document.write("<h1>hello</h1>");
+document.write("<h5>hood morning</h5>");
+var name="aan";
+document.write(name);
+document.write("<br>")
+let student={ name:'chaithanya',age:'23'};
+document.write(student.name);
+array=['anu','amritha'];
+document.write(array);
+document.write(array[1])
